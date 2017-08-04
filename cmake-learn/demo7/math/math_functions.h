e7d7e723a4a3486a84785979a736ebd64534ce78
zsb <1109179198@qq.com>
2017-08-04 17:26:03 +0800

    test

---
+++ cmake-learn/demo7/math/math_functions.h
@@ -0,0 +1,13 @@
+//
+// Created by zsb on 17/8/3.
+//
+
+#ifndef CPPLEARN_MATH_FUNCTIONS_H
+#define CPPLEARN_MATH_FUNCTIONS_H
+
+
+extern double power(double  base, int exponent);
+
+
+#endif //CPPLEARN_MATH_FUNCTIONS_H
+
---
+++ cmake-learn/demo8/main.cc
@@ -0,0 +1,42 @@
+//
+// Created by zsb on 17/8/3.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "config.h"
+
+#ifdef  HAVE_POW
+#include <math.h>
+#else
+
+#include <math_functions.h>
+
+#endif
+
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        // print version info
+        printf("%s Version %d.%d\n",
+               argv[0],
+               DEMO_VERSION_MAJOR,
+               DEMO_VERSION_MINOR);
+        printf("Usage: %s base exponent \n", argv[0]);
+        return 1;
+    }
+    double base = atof(argv[1]);
+    int exponent = atoi(argv[2]);
+
+#ifdef HAVE_POW
+    printf("Now we use standard libraray. \n");
+    double result = pow(base, exponent);
+#else
+    printf("Now we use our own Math libraray. \n");
+    double result = power(base, exponent);
+#endif
+
+    printf("%g ^ %d is %g\n", base, exponent, result);
+
+    return 0;
+}
